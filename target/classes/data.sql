846518dfd93835c379ed8a12fa1518a43ff92374
Tao1925 <yhxxtsy@163.com>
2025-10-22 21:57:31 +0800

    251022去除无关文件

--- target/classes/data.sql
+++
@@ -1,3 +0,0 @@
--- 插入测试用户数据
-INSERT INTO users (username, password) VALUES ('admin', '123456');
-
--- target/classes/schema.sql
+++
@@ -1,6 +0,0 @@
--- 创建用户表
-CREATE TABLE IF NOT EXISTS users (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
